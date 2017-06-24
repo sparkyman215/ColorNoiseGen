98b632b9c151a8a8e08ac647a89683bcc8dc3a6b
sparkyman215 <pmcpokemon@gmail.com>
2017-06-23 20:30:48 -0400

    Initial commit

---
+++ src/idk/no.java
@@ -0,0 +1,77 @@
+package idk;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+public class no {
+
+	
+	public static int corneravg(int[][] takeme, int tempk, int tempi) { // a very specific function that averages the four corners of a array location
+		
+		int avg = 0; // initialize because eclipse errors are annoying as shit
+		avg = ((takeme[tempk-1][tempi]) +
+				(takeme[tempk+1][tempi]) +
+				(takeme[tempk][tempi-1]) +
+				(takeme[tempk][tempi+1])) /
+				4;
+		
+		int randomizer = (int)(Math.random()*20);
+		
+		if (Math.random()*50 < 25) {
+			avg -= randomizer;
+		} else {
+			avg += randomizer; // needs a random addition otherwise it'll just be zeroes xd
+		}
+		
+		if (avg > 255) { avg = 255; }
+		else if (avg < 0) { avg = 0; }
+		
+		return avg;
+	}
+	
+	public static int[][] colorGen(int size) {
+		
+		int[][] arr = new int[size+2][5];
+		int randN = 170;
+		int temp[] = {0,((int)(Math.random()*randN+170)),((int)(Math.random()*randN)),((int)(Math.random()*randN))};
+		arr[1] = Arrays.copyOf(temp, 5);
+		
+		Color tempC; int rgb=0; 
+		BufferedImage img = new BufferedImage(size+1,size+1,BufferedImage.TYPE_INT_RGB);
+		
+		for (int xd = 0; xd < size-2; xd++) {
+			for(int k = 2; k <= size; k++) {
+				int iPerm = 0;
+				for(int i = 1; i <= 3; i++) {
+					arr[k][i] = corneravg(arr, k, i);
+					iPerm = i;
+				}
+				tempC = new Color(arr[k][1],arr[k][2],arr[k][3]);
+				rgb = tempC.getRGB();
+				img.setRGB(k,iPerm+xd,rgb);
+			}
+		}
+		
+		
+		//File output = new File("saved" + Calendar.getInstance().getTimeInMillis() + ".png");
+		File output = new File("output.png");
+		try {
+			ImageIO.write(img, "png", output);			
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		
+		//System.out.println(Arrays.deepToString(colorGen(512)));
+		
+	}
+}
---
+++ src/idk/progrum.java
@@ -0,0 +1,88 @@
+package idk;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Image;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class progrum extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					progrum frame = new progrum();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public progrum() {
+		
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 540, 592);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("output.png"));
+		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+		JButton btnGenerate = new JButton("Generate");
+		btnGenerate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnGenerate.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				int size = 128;
+				no.colorGen(size);
+				
+				
+				ImageIcon icon = new ImageIcon("output.png");
+				Image image = icon.getImage();
+				Image newimg = image.getScaledInstance(size*4, size*4, java.awt.Image.SCALE_DEFAULT);
+				icon = new ImageIcon(newimg);
+				lblNewLabel.setIcon(icon);
+				icon.getImage().flush();
+				lblNewLabel.repaint();
+			}
+		});
+		contentPane.add(btnGenerate, BorderLayout.SOUTH);
+		
+		
+	}
+	
+	
+
+}
